ecf91aebf73e8f95c1c14d8f53b51d741ac15df6
Jamal Uddin Ahamed <68894666+jamaluddinahamed@users.noreply.github.com>
2021-10-10 13:48:11 +0530

    a

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, {Component } from 'react'
 import './App.css';
 import Greet from './components/greet';
 import Welcome from './components/welcome';
+import Hello from './components/hello';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Greet />
-        <Welcome />
+        {/*<Greet />
+        <Welcome />*/}
+        <Hello />
       </div>
     );
   }
